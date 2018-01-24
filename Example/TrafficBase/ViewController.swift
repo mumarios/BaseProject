50ceab02c13f6f4fd94dbd94312b91997ec1267e
faraz <faraz@Faraz-Siddiqui.local>
2018-01-24 12:01:47 +0500

    Initial commit

---
+++ Example/TrafficBase/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TrafficBase
+//
+//  Created by FaraziOS on 01/24/2018.
+//  Copyright (c) 2018 FaraziOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
